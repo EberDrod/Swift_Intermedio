4cd1e1ade1b06acfed1aa2f038f49fe3b10ecee5
Eber Peña <eberg.pena@gmail.com>
2020-11-06 15:27:04 -0600

    Se ha creado fucniones con un slo parametro

---
+++ Funciones.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+func holaMundo(){
+    print("Hola Mundo")
+}
+
+holaMundo()
+
+//Funciones de un solo parametro
+
+
+func cuadradoDe(numero:Int){
+    
+    let numeroCuadrado = numero + numero
+    print(numeroCuadrado)
+}
+
+
+cuadradoDe(numero: 10)
